bdd6b1383b1db3a2533dacbe8ec2034d721fbeac
hankwojteczkodki <hankwojteczko@davidkentconsulting.com>
2022-12-15 14:01:22 -0600

    Working on getting AZ DevOps pipeline working for OCI terraform

--- setup.sh
+++ setup.sh
@@ -6,8 +6,3 @@
 /bin/chmod 700 $HOME/.oci
 /bin/ls -la $HOME
 
-# install OCI CLI tools
-/bin/wget -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh > /tmp/install.sh
-/bin/chmod 500 /tmp/install.sh
-/tmp/install.sh --accept-all-defaults
-/bin/sleep 180
