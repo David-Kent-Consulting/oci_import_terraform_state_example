b20a611e000d61ddf03d4fb7080367a98bdfa06b
hankwojteczkodki <hankwojteczko@davidkentconsulting.com>
2022-12-15 14:29:11 -0600

    Working on getting AZ DevOps pipeline working for OCI terraform

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# this is set by Az DevOps, user must be vsts
+export OCI_HOME=/home/vsts/lib/oracle-cli/bin/oci
 
 # create the OCI config directory
 /bin/mkdir $HOME/.oci
 /bin/chmod 700 $HOME/.oci
 /bin/ls -la $HOME
 
+# copy the config and API keys, this action to be replaced by Azure Files
+# after consultation with Rel.
+
+
 # test the oci cli tools
-/home/vsts/lib/oracle-cli/bin/oci --version
+$OCI_HOME/oci --version
+
