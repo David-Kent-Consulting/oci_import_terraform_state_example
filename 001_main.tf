3050bd6e166f7a6c61b9a8e1eecc471371b57e19
hankwojteczkodki <hankwojteczko@davidkentconsulting.com>
2022-12-15 16:20:38 -0600

    Working on getting AZ DevOps pipeline working for OCI terraform

--- 001_main.tf
+++ 001_main.tf
@@ -1,7 +1,7 @@
 terraform {
 
 	backend "http" {
-		address = "Your Storage Pre-Authentication URL plus the name of the terraform state key file. Below is an example"
+		address = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/6VPJ4QelMOdUyHMVtZ3IFe-fXawu7o65okE30IGAe3vo06WI6mQo1JH86Hm_0jHa/n/idp6wbidjntn/b/wojteczko_tf_poc/o/dbs_comp.tfstate"
 		# address = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/Oe6HuLvwOlSLYp8HOciIl1uM6-dlCeDi1289048fBdY5AWkb0O_m2gCe4J1sum/n/idp6wbidjntn/b/example_tf_poc/o/dbs_comp.tfstate"
 		update_method = "PUT"
 	}	
