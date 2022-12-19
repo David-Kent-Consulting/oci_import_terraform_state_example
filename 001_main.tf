8491383bb5349752f56225f152fc1f8fe337a1c0
hankwojteczkodki <hankwojteczko@davidkentconsulting.com>
2022-12-19 08:01:19 -0600

    testing

--- 001_main.tf
+++ 001_main.tf
@@ -1,7 +1,7 @@
 terraform {
 
 	backend "http" {
-		address = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/6VPJ4QelMOdUyHMVtZ3IFe-fXawu7o65okE30IGAe3vo06WI6mQo1JH86Hm_0jHa/n/idp6wbidjntn/b/wojteczko_tf_poc/o/dbs_comp.tfstate"
+		address = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/qUKcDpQ9-WyGmfZjVBa-xf7ezGKqKJ17aagHgQNjDa66CrqeYNSZvas450u8QTMY/n/idp6wbidjntn/b/wojteczko_tf_poc/o/"
 		# address = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/Oe6HuLvwOlSLYp8HOciIl1uM6-dlCeDi1289048fBdY5AWkb0O_m2gCe4J1sum/n/idp6wbidjntn/b/example_tf_poc/o/dbs_comp.tfstate"
 		update_method = "PUT"
 	}	
