828a027acf88e561266266b920676f287e095eff
hankwojteczkodki <hankwojteczko@davidkentconsulting.com>
2022-12-19 15:13:06 -0600

    still testing

--- vars.tf
+++ vars.tf
@@ -1,3 +1,6 @@
-variable tenancy_ocid { default = "ocid1.tenancy.oc1..aaaaaaaaoqsm6s5l4fgiurqfj6kgjewdeiesxzzgr5zyjmwiterd3qdfacfa" }
-variable compartment_ocid { default = "ocid1.tenancy.oc1..aaaaaaaaoqsm6s5l4fgiurqfj6kgjewdeiesxzzgr5zyjmwiterd3qdfacfa" }
+variable availability_domain--clNq-US-ASHBURN-AD-2 { default = "clNq:US-ASHBURN-AD-2" }
+variable fault_domain--FAULT-DOMAIN-2 { default = "FAULT-DOMAIN-2" }
 variable region { default = "us-ashburn-1" }
+variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaaajog445tn7gldsjs5brfum4wctrgwxwnhze3w5i4ycsxzui4exna" }
+variable oci_core_instance--source_details-source_id--export_kentemst01_1 { default = "ocid1.image.oc1.iad.aaaaaaaak6w63p4ddsxlh2hgqh2vycoivsdevwy2utwsqjxwcvsj4vfstlqa" }
+variable availability_domain--clNq-US-ASHBURN-AD-3 { default = "clNq:US-ASHBURN-AD-3" }
